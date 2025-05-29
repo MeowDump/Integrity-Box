18a6d3d29a27b6a726e9af1ae6c0646bca0c6ea9
MONA / <121730102+MeowDump@users.noreply.github.com>
2025-05-29 16:48:58 +0530

    Update service.sh

--- service.sh
+++ service.sh
@@ -1,25 +1,45 @@
 #!/system/bin/sh
+
 # Meow
 MEOW() {
     am start -a android.intent.action.MAIN -e mona "$@" -n meow.helper/.MainActivity &>/dev/null
     sleep 0.5
 }
 
-MCTRL="${0%/*}"; WHITELIST="/data/adb/shamiko/whitelist"
+# Check if Magisk is present
+is_magisk() {
+    [ -d /data/adb/magisk ] || getprop | grep -q 'magisk'
+}
+
+MCTRL="${0%/*}"
+SHAMIKO_WHITELIST="/data/adb/shamiko/whitelist"
+NOHELLO_DIR="/data/adb/nohello"
+NOHELLO_WHITELIST="$NOHELLO_DIR/whitelist"
+
 while true; do
  if [ ! -e "${MCTRL}/disable" ] && [ ! -e "${MCTRL}/remove" ]; then
-  [ ! -f "$WHITELIST" ] && touch "$WHITELIST" && MEOW "Whitelist Mode Activated.✅"
+  if is_magisk && [ ! -f /sdcard/stop ]; then
+    if [ ! -f "$SHAMIKO_WHITELIST" ]; then
+      touch "$SHAMIKO_WHITELIST"
+      MEOW "Shamiko Whitelist Mode Activated.✅"
+    fi
+    if [ -d "$NOHELLO_DIR" ] && [ ! -f "$NOHELLO_WHITELIST" ]; then
+      touch "$NOHELLO_WHITELIST"
+      MEOW "NoHello Whitelist Mode Activated.✅"
+    fi
+  fi
  else
-  [ -f "$WHITELIST" ] && rm "$WHITELIST" && MEOW "Blacklist Mode Activated.❌"
+  [ -f "$SHAMIKO_WHITELIST" ] && rm "$SHAMIKO_WHITELIST" && MEOW "Shamiko Blacklist Mode Activated.❌"
+  [ -f "$NOHELLO_WHITELIST" ] && rm "$NOHELLO_WHITELIST" && MEOW "NoHello Blacklist Mode Activated.❌"
  fi
  sleep 4
-done
+done &
 
 # Define module path dynamically
 export MODPATH="/data/adb/modules/Integrity-Box"
 
 # Remove LineageOS props
-#Credits- @ez-me for https://github.com/ez-me/ezme-nodebug
+# Credits - @ez-me for https://github.com/ez-me/ezme-nodebug
 resetprop --delete ro.lineage.build.version
 resetprop --delete ro.lineage.build.version.plat.rev
 resetprop --delete ro.lineage.build.version.plat.sdk
@@ -34,8 +54,8 @@ getprop | grep "userdebug" >> "$MODPATH/tmp.prop"
 getprop | grep "test-keys" >> "$MODPATH/tmp.prop"
 getprop | grep "lineage_"  >> "$MODPATH/tmp.prop"
 
-sed -i 's/\[//g'  "$MODPATH/tmp.prop"
-sed -i 's/\]//g'  "$MODPATH/tmp.prop"
+sed -i 's///g'  "$MODPATH/tmp.prop"
+sed -i 's///g'  "$MODPATH/tmp.prop"
 sed -i 's/: /=/g' "$MODPATH/tmp.prop"
 
 sed -i 's/userdebug/user/g' "$MODPATH/tmp.prop"
@@ -63,4 +83,3 @@ resetprop ro.boot.realme.lockstate 1
 resetprop ro.bootmode unknown
 resetprop ro.boot.bootmode unknown
 resetprop vendor.boot.bootmode unknown
-
