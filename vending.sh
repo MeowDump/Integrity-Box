932d2398da6a4f4fd2fc1e56b86d3c3da47c18bf
MONA / <121730102+MeowDump@users.noreply.github.com>
2025-06-08 01:08:42 -0700

    ㅤ

--- vending.sh
+++ vending.sh
@@ -1,4 +1,4 @@
-MEOW() {
+popup() {
     am start -a android.intent.action.MAIN -e mona "$@" -n meow.helper/.MainActivity &>/dev/null
     sleep 0.5
 }
@@ -6,5 +6,5 @@ MEOW() {
 su -c "pm clear com.android.vending"
 su -c "pm clear com.google.android.gms"
 
-MEOW " Open playstore & Check/FIX"
+popup " Open playstore & Check/FIX"
 exit 0
