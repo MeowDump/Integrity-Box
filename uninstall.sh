6efeb5ee5324db438cad6d0c4340f39fbbf9d347
MONA / <121730102+MeowDump@users.noreply.github.com>
2025-06-19 05:55:29 +0530

    ‎

--- uninstall.sh
+++ uninstall.sh
@@ -3,4 +3,3 @@ rm -rf /data/adb/tricky_store/keybox.xml
 rm -rf /data/adb/tricky_store/keybox.xml.bak
 rm -rf /data/adb/tricky_store/target.txt
 rm -rf /data/adb/modules/integrity_box
-rm -rf /data/adb/modules/susfs4ksu/action.sh
