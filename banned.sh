87ea6c9ee8f2c829aa69d88cd115cd626e92e77b
MONA / <121730102+MeowDump@users.noreply.github.com>
2025-06-18 18:00:18 -0700

    Delete banned.sh

--- banned.sh
+++
@@ -1,9 +0,0 @@
-MEOW() {
-    am start -a android.intent.action.MAIN -e mona "$@" -n meow.helper/.MainActivity &>/dev/null
-    sleep 0.5
-}
-
-nohup am start -a android.intent.action.VIEW -d https://android.googleapis.com/attestation/status >/dev/null 2>&1 &
-MEOW "Redirecting to Google Revoked List"
-
-exit 0
