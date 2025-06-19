74c8811f444c174a81aa7ec9f9721d6d89354687
MONA / <121730102+MeowDump@users.noreply.github.com>
2025-06-19 05:47:10 +0530

    ‎

--- webroot/common_scripts/issue.sh
+++ webroot/common_scripts/issue.sh
@@ -1,9 +1,9 @@
-#!/system/bin/sh
-
 popup() {
     am start -a android.intent.action.MAIN -e mona "$@" -n meow.helper/.MainActivity &>/dev/null
     sleep 0.5
 }
 
-popup "This feature is under maintenance"
+nohup am start -a android.intent.action.VIEW -d https://t.me/TempMeow >/dev/null 2>&1 & 
+popup "Report your problem here"
+
 exit 0
--- webroot/common_scripts/vending.sh
+++ webroot/common_scripts/vending.sh
@@ -1,9 +1,10 @@
-#!/system/bin/sh
-
 popup() {
     am start -a android.intent.action.MAIN -e mona "$@" -n meow.helper/.MainActivity &>/dev/null
     sleep 0.5
 }
 
-popup "This feature is under maintenance"
+su -c "pm clear com.android.vending"
+su -c "pm clear com.google.android.gms"
+
+popup " Open playstore & Check/FIX"
 exit 0
