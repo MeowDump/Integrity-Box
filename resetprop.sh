47aa5ca70820b820b568476e3c3a81061e863d80
MONA / <121730102+MeowDump@users.noreply.github.com>
2025-06-18 18:04:49 -0700

    Delete resetprop.sh

--- resetprop.sh
+++
@@ -1,9 +0,0 @@
-MEOW() {
-    am start -a android.intent.action.MAIN -e mona "$@" -n meow.helper/.MainActivity &>/dev/null
-    sleep 0.5
-}
-
-su -c "resetprop -p -d persist.sys.pihooks.disable.gms_props"
-su -c "resetprop -p -d persist.sys.pihooks.disable.gms_key_attestation_block"
-
-MEOW "Switched back to default settings"
