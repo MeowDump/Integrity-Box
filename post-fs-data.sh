9aa49248112bd4fe96fc8bb1438a0cbc715cda66
MONA <121730102+MeowDump@users.noreply.github.com>
2026-08-01 00:07:01 -0700

    Improved paths and permissions
    Signed-off-by: MONA <121730102+MeowDump@users.noreply.github.com>

--- post-fs-data.sh
+++ post-fs-data.sh
@@ -2,22 +2,28 @@
 MODPATH="${0%/*}"
 . $MODPATH/common_func.sh
 
+BOX="/data/adb/Box-Brain"
 boot="/data/adb/service.d"
 placeholder="$MODPATH/webroot/common_scripts"
-mkdir -p "/data/adb/Box-Brain/Integrity-Box-Logs"
+mkdir -p "$BOX/Integrity-Box-Logs"
 mkdir -p "$boot"
 
+# Grant perms 
+if [ -f "$placeholder/autopilot.sh" ]; then
+    chmod 755 "$placeholder/autopilot.sh"
+fi
+
 # Handle Vending-specific prop
-if [ -f "/data/adb/Box-Brain/enablevending" ]; then
+if [ -f "$BOX/enablevending" ]; then
     set_simpleprop persist.sys.pixelprops.vending true
 fi
 
-if [ -f "/data/adb/Box-Brain/disablevending" ]; then
+if [ -f "$BOX/disablevending" ]; then
     set_simpleprop persist.sys.pixelprops.vending false
 fi
 
 # Handle GMS-specific props
-if [ -f "/data/adb/Box-Brain/enablegms" ]; then
+if [ -f "$BOX/enablegms" ]; then
     setprop persist.sys.pihooks.disable.gms_key_attestation_block false
     setprop persist.sys.pihooks.disable.gms_props false
     setprop persist.sys.pihooks.enabled_features 1
@@ -25,7 +31,7 @@ if [ -f "/data/adb/Box-Brain/enablegms" ]; then
     setprop persist.sys.kihooks.disable 0
 fi
 
-if [ -f "/data/adb/Box-Brain/disablegms" ]; then
+if [ -f "$BOX/disablegms" ]; then
     setprop persist.sys.pihooks.disable.gms_key_attestation_block true
     setprop persist.sys.pihooks.disable.gms_props true
     setprop persist.sys.pihooks.enabled_features 0
@@ -45,6 +51,7 @@ for _f in \
     "$boot/lineage.sh" \
     "$boot/package.sh" \
     "$boot/.box_cleanup.sh" \
+    "$placeholder/autopilot.sh" \
     "$placeholder/target.sh" \
     "$placeholder/gms.sh" \
     "$placeholder/webui.sh" \
@@ -55,11 +62,18 @@ for _f in \
     "$placeholder/force_override.sh" \
     "$placeholder/override_lineage.sh" \
     "$placeholder/keymint.sh" \
-    "$placeholder/kernel.sh" \
     "$placeholder/hma.sh"
 do
     set_perm_if_needed "$_f" 755
 done
+
+if [ -e "$BOX/ota" ]; then
+    rm -f "$MODPATH/system.prop"
+    rm -f "$BOX/NoLineageProp"
+    rm -rf "$BOX/override"
+    rm -rf "$BOX/ota"
+    touch "$BOX/safemode"
+fi
 
 ##########################################
 # adapted from Play Integrity Fork by @osm0sis
@@ -72,13 +86,16 @@ if [ -d "/data/adb/magisk" ]; then
     echo "Magisk detected."
 
     if [ -d "$MODPATH/zygisk" ]; then
+        # Remove Play Services and Play Store from Magisk DenyList when set to Enforce in normal mode
         if magisk --denylist status; then
             magisk --denylist rm com.google.android.gms
             magisk --denylist rm com.android.vending
         fi
 
+        # Run common tasks for installation and boot-time
         . "$MODPATH/common_setup.sh"
     else
+        # Add Play Services DroidGuard and Play Store processes to Magisk DenyList for better results in scripts-only mode
         magisk --denylist add com.google.android.gms com.google.android.gms.unstable
         magisk --denylist add com.android.vending
     fi
@@ -90,44 +107,34 @@ fi
 # Conditional early sensitive properties
 
 # Samsung
-$RESETPROP_RS --stealth ro.boot.warranty_bit 0
-$RESETPROP_RS --stealth ro.vendor.boot.warranty_bit 0
-$RESETPROP_RS --stealth ro.vendor.warranty_bit 0
-$RESETPROP_RS --stealth ro.warranty_bit 0
+resetprop_if_diff ro.boot.warranty_bit 0
+resetprop_if_diff ro.vendor.boot.warranty_bit 0
+resetprop_if_diff ro.vendor.warranty_bit 0
+resetprop_if_diff ro.warranty_bit 0
 
 # Realme
-$RESETPROP_RS --stealth ro.boot.realmebootstate green
+resetprop_if_diff ro.boot.realmebootstate green
 
 # OnePlus
-$RESETPROP_RS --stealth ro.is_ever_orange 0
+resetprop_if_diff ro.is_ever_orange 0
 
 # Microsoft
-for PROP in $($RESETPROP_RS | grep -oE 'ro.*.build.tags'); do
-    $RESETPROP_RS --stealth "$PROP" release-keys
+for PROP in $(resetprop | grep -oE 'ro.*.build.tags'); do
+    resetprop_if_diff $PROP release-keys
 done
 
 # Other
-for PROP in $($RESETPROP_RS | grep -oE 'ro.*.build.type'); do
-    $RESETPROP_RS --stealth "$PROP" user
+for PROP in $(resetprop | grep -oE 'ro.*.build.type'); do
+    resetprop_if_diff $PROP user
 done
-$RESETPROP_RS --stealth ro.adb.secure 1
+resetprop_if_diff ro.adb.secure 1
 if ! $SKIPDELPROP; then
-    $RESETPROP_RS --nuke ro.boot.verifiedbooterror
-    $RESETPROP_RS --nuke ro.boot.verifyerrorpart
+    delprop_if_exist ro.boot.verifiedbooterror
+    delprop_if_exist ro.boot.verifyerrorpart
 fi
-$RESETPROP_RS --stealth ro.boot.veritymode.managed yes
-$RESETPROP_RS --stealth ro.debuggable 0
-$RESETPROP_RS --stealth ro.force.debuggable 0
-$RESETPROP_RS --stealth ro.secure 1
-
-
-# Hide custom ROM
-if [ -f "/data/adb/Box-Brain/kernel" ]; then
-   log "Detected kernel flag"
-   sh "$placeholder/kernel.sh"
-   log "kernel.sh executed successfully"
-else
-   log "kernel spoofer flag not found, skipping kernel.sh"
-fi
+resetprop_if_diff ro.boot.veritymode.managed yes
+resetprop_if_diff ro.debuggable 0
+resetprop_if_diff ro.force.debuggable 0
+resetprop_if_diff ro.secure 1
 
 exit 0
